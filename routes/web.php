43bb7d627c5be2fae5b145ff33ac9a4eb248e71d
marcoaufiero <marcoaufiero96@gmail.com>
2023-01-24 13:00:00 +0100

    routes edited in web.php

--- routes/web.php
+++ routes/web.php
@@ -13,10 +13,25 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+// Route::get('/', function () {
+//     return view('welcome');
+// });
+
+// Auth::routes();
+
+// Route::get('/home', 'HomeController@index')->name('home');
 
 Auth::routes();
 
-Route::get('/home', 'HomeController@index')->name('home');
+Route::middleware('auth')
+    ->namespace('Admin')
+    ->prefix('admin')
+    ->name('admin.')
+    ->group(function(){
+        Route::get('/', 'HomeController@index')->name('index');
+        Route::resource('/posts', PostController::class);
+});
+
+Route::get('{any?}', function(){
+    return view('guest.home');
+})->where("any", ".*");
